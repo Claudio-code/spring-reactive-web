7393e93f5b673fff64e7dafef662c53f206e0d48
“claudio-code” <suporte2424@outlook.com>
2022-08-25 21:53:38 -0300

    added examples how test reactive api

--- src/test/java/com/reactive/reactive/learning/webclient/PostRequestTest.java
+++ src/test/java/com/reactive/reactive/learning/webclient/PostRequestTest.java
@@ -1,38 +1,33 @@
 package com.reactive.reactive.learning.webclient;
 
 import com.reactive.reactive.learning.ApplicationTests;
+import com.reactive.reactive.learning.dto.MultiplyRequestDTO;
 import com.reactive.reactive.learning.dto.ResponseDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.test.StepVerifier;
 
-public class GetSingleResponseTest extends ApplicationTests {
+public class PostRequestTest extends ApplicationTests {
 
     @Autowired
     private WebClient webClient;
 
     @Test
-    void blockTest() {
-        var response = webClient.get()
-                .uri("reactive/math/square/{number}", 5)
-                .retrieve()
-                .bodyToMono(ResponseDTO.class)
-                .block();
-
-        System.out.println(response);
-    }
-
-    @Test
-    void stepVerifierTest() {
-        var responseDTOMono = webClient.get()
-                .uri("reactive/math/square/{number}", 5)
+    void stepVerifierFluxTest() {
+        var responseDTOMono = webClient.post()
+                .uri("reactive/math")
+                .bodyValue(buildMultiplyRequestDTO(5, 2))
                 .retrieve()
                 .bodyToMono(ResponseDTO.class);
 
         StepVerifier.create(responseDTOMono)
-                .expectNextMatches(r -> r.getOutput() == 25)
+                .expectNextCount(1)
                 .verifyComplete();
+    }
+
+    private MultiplyRequestDTO buildMultiplyRequestDTO(int a, int b) {
+        return new MultiplyRequestDTO(a, b);
     }
 
 }
